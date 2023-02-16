b2317f7519a34d7c455fcb48517dbc1665109499
Mukesh Rathod <mukeshrathod757@gmail.com>
2023-02-16 15:39:36 +0530

    Added new files

---
+++ src/scmtraingEdureka/TestJava.java
@@ -0,0 +1,13 @@
+package scmtraingEdureka;
+
+public class TestJava {
+	public void m1() {
+		System.out.println("from m1");
+	}
+	public static void main(String[] args) {
+		TestJava obj = new TestJava();
+		obj.m1();
+		
+	}
+
+}
